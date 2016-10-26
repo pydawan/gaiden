a67f7256ad7619d46114fdd4c1edbaf399295314
Eduardo Matos <eduardoj.matos@gmail.com>
2016-10-26 12:53:10 -0200

    Using kss to generate our styleguide

---
+++ .browser-sync-docs.js
@@ -0,0 +1,25 @@
+
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+  ui: false,
+  files: "styleguide/*",
+  server: {
+    baseDir: "styleguide",
+    directory: true
+  },
+  port: 4000,
+  open: false,
+  cors: true
+};
---
+++ bin/docs/kss.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+
+const shelljs = require('shelljs');
+
+const configFile = './.kss-config.json';
+const cssMinified = './.kss-config.json';
+const outputDir = "./build/stylesheets";
+const minifiedFile = `${outputDir}/ninja-gaiden.css`;
+const styleguidePath = './styleguide';
+
+shelljs.exec(`kss --config ${configFile}`);
+shelljs.cp(`${minifiedFile}`, styleguidePath);
